b12e855595ff33c85829c3051259aeaff261b7f9
UserUsesGithub <127356804+UserUsesGithub@users.noreply.github.com>
2026-05-02 09:15:36 -0400

    Update library.js

--- library.js
+++ library.js
@@ -1,11 +1,11 @@
-// Run this in the console and it will generate variables for W, A, S and D
+// Run this in the console and it will make keys from VALUE into desmos variables
 document.addEventListener('keydown', (event) => {
-    if (["w","a","s","d"].includes(event.key)) {
+    if (VALUE.includes(event.key)) {
         Calc.setExpression({id:event.code,latex:`K_{EYREADINGLIB${event.code}}=1`})
     }
 });
 document.addEventListener('keyup', (event) => {
-    if (["w","a","s","d"].includes(event.key)) {
+    if (VALUE.includes(event.key)) {
         Calc.setExpression({id:event.code,latex:`K_{EYREADINGLIB${event.code}}=0`})
     }
 });
