7f0c39f013e3b5df898f1866df6976ad61d69288
UserUsesGithub <127356804+UserUsesGithub@users.noreply.github.com>
2026-05-02 09:11:24 -0400

    Update library.js

--- library.js
+++ library.js
@@ -1,11 +1,11 @@
 // Run this in the console and it will generate variables for W, A, S and D
 document.addEventListener('keydown', (event) => {
-    if (["KeyW","KeyA","KeyS","KeyD"].includes(event.code)) {
+    if (["w","a","s","d"].includes(event.key)) {
         Calc.setExpression({id:event.code,latex:`K_{EYREADINGLIB${event.code}}=1`})
     }
 });
 document.addEventListener('keyup', (event) => {
-    if (["KeyW","KeyA","KeyS","KeyD"].includes(event.code)) {
+    if (["w","a","s","d"].includes(event.key)) {
         Calc.setExpression({id:event.code,latex:`K_{EYREADINGLIB${event.code}}=0`})
     }
 });
